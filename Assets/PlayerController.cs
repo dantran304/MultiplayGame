90b56e42af0c70b88ebadf6c64e521887538525d
dantran304 <dantrandinhh@gmail.com>
2016-05-10 00:30:42 +0700

    part 6

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 100;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * 100;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+	}
+}
