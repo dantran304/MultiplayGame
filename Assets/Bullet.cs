d654e8e8791bbe4a2a922ab1a5b4fa1f113209bd
dantran304 <dantrandinhh@gmail.com>
2016-05-11 00:31:21 +0700

    add bullet

---
+++ Assets/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SS
+{
+    public class Bullet : MonoBehaviour {
+
+      
+    void OnCollisionEnter()
+    {
+        Destroy(gameObject);
+    }
+}
+}
+
+
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour {
 
-	
+    public int runSpeed;
+    public int rotationSpeed;
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public int bulletSpeed;
 	// Update is called once per frame
 	void Update () {
 
@@ -12,12 +16,31 @@ public class PlayerController : NetworkBehaviour {
         {
             return;
         }
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 100;
-        float z = Input.GetAxis("Vertical") * Time.deltaTime * 100;
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * runSpeed;
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
 	}
+
+    [Command]
+    void CmdFire()
+    {
+        GameObject bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
+
+        //spawn bullet on the client
+        NetworkServer.Spawn(bullet);
+
+        Destroy(bullet, 2);
+    }
 
     public override void OnStartLocalPlayer()
     {
